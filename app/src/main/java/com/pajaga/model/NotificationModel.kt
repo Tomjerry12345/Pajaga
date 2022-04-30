85d24c88258d1bcca7880ed69a66b6b90631d15c
ASUS <Andrialfiandi46@gmail.com>
2022-04-30 23:35:05 +0800

    fix: notif click move to profil

--- app/src/main/java/com/pajaga/model/NotificationModel.kt
+++ app/src/main/java/com/pajaga/model/NotificationModel.kt
@@ -2,6 +2,12 @@ package com.pajaga.model
 
 import com.pajaga.utils.other.Constant
 
+data class PushNotification(
+    val data: NotificationData,
+    val to: String,
+    val notification: Notification
+)
+
 data class NotificationData(
     val title: String,
     val message: String
@@ -13,10 +19,4 @@ data class Notification(
     val android_channel_id: String = Constant.CHANNEL_ID,
     val sound : String = "notification2",
     val click_action: String = "ProfilActivity"
-)
-
-data class PushNotification(
-    val data: NotificationData,
-    val to: String,
-    val notification: Notification
 )
